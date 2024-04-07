a2a93106c7a5f54de755faf24554b42c0731f7b8
Guy Rozanes <guyrozanes18@gmail.com>
2024-04-07 16:00:47 +0300

    change api program

--- InfraEdge.API/Program.cs
+++ InfraEdge.API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using InfraEdge.API.InfraEdge.API.Data;
 using InfraEdge.API.InfraEdge.API.Data.WikiApiClient;
+using InfraEdge.API.InfraEdge.API.Logic;
 using System;
 using System.Collections.Generic;
 
@@ -8,6 +9,10 @@ class Program
 {
     static void Main(string[] args)
     {
-      
+        var wikiLogic = new WikiApiLogic();
+        foreach (var pair in wikiLogic.GetAllSpecialWordsOccurrences().GetAwaiter().GetResult())
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
     }
 }
